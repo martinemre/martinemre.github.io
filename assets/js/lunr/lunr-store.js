8f1d8d4e14a55f0737532aa540bc5cbc59c1709a
martinemre <martinemre@users.noreply.github.com>
2026-04-05 13:45:36 +0000

    deploy: martinemre/devcode@7ab118ba3f73f1c06824f8415ccd7108341dd887

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -38,9 +38,6 @@ var store = [{
     "title": "Archive",
     "excerpt":"","url": "https://martinemre.github.io/year-archive/"
   },{
-    "title": null,
-    "excerpt":"@import \"jekyll-theme-primer\"; ","url": "https://martinemre.github.io/assets/css/style.css"
-  },{
     "title": "Table of contents",
     "excerpt":"# Table of contents  * [Page](README.md)  ## Group 1  * [Page 3](group-1/page-3/README.md)   * [Page 4](group-1/page-3/page-4/README.md)     * [Page 2](group-1/page-3/page-4/page-2.md)     * [Page 1](group-1/page-3/page-4/page-1.md)  ***  * [Page 5](page-5.md) ","url": "https://martinemre.github.io/SUMMARY/"
   },{
